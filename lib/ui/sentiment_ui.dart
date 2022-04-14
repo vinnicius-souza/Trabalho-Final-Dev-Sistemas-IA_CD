b40cedeab84564ab408020de280ac20b89b80994
Vinnicius Souza <vinniciussouza100@gmail.com>
2022-04-14 20:33:34 -0300

    final

--- lib/ui/sentiment_ui.dart
+++ lib/ui/sentiment_ui.dart
@@ -11,7 +11,8 @@ class SentimentAnalyzer extends StatefulWidget {
 class _SentimentAnalyzerState extends State<SentimentAnalyzer> {
   final TextEditingController _textController = TextEditingController();
   String _sentiment = "";
-  String _result_image = "";
+  String _result_image = "assets/logo_maua.jpg";
+  String _current_language = "en";
 
   @override
   Widget build(BuildContext context) {
@@ -64,6 +65,38 @@ class _SentimentAnalyzerState extends State<SentimentAnalyzer> {
                       primary: Colors.white,
                       textStyle: const TextStyle(fontSize: 24),
                     ),
+                    onPressed: () {
+                      if (_current_language == 'en') {
+                        _current_language = 'pt';
+                      } else {
+                        _current_language = 'en';
+                      }
+                      setState(() {});
+                    },
+                    child: Text(_current_language),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.only(right: 32, left: 32),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      color: const Color(0xff004586),
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 24),
+                    ),
                     onPressed: () async {
                       var dio = Dio();
                       var response = await dio.post(
@@ -71,7 +104,7 @@ class _SentimentAnalyzerState extends State<SentimentAnalyzer> {
                         data: {
                           "documents": [
                             {
-                              "language": "pt",
+                              "language": _current_language,
                               "id": "1",
                               "text": _textController.text,
                             },
@@ -79,8 +112,7 @@ class _SentimentAnalyzerState extends State<SentimentAnalyzer> {
                         },
                         options: Options(
                           headers: {
-                            "Ocp-Apim-Subscription-Key":
-                                "67260ed3ffb4468ab513787ca491f629",
+                            "Ocp-Apim-Subscription-Key": "<chave aqui>",
                           },
                         ),
                       );
@@ -88,6 +120,13 @@ class _SentimentAnalyzerState extends State<SentimentAnalyzer> {
                         () {
                           _sentiment =
                               response.data["documents"][0]["sentiment"];
+                          if (_sentiment == "positive") {
+                            _result_image = "assets/happy.jpg";
+                          } else if (_sentiment == "neutral") {
+                            _result_image = "assets/neutral.png";
+                          } else if (_sentiment == "negative") {
+                            _result_image = "assets/sad.webp";
+                          }
                         },
                       );
                     },
@@ -105,7 +144,7 @@ class _SentimentAnalyzerState extends State<SentimentAnalyzer> {
             ),
           ),
           Image.asset(
-            'assets/logo_maua.jpg',
+            _result_image,
             height: 40,
             width: 40,
             fit: BoxFit.contain,
